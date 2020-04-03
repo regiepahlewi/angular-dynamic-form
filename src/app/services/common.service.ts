d7cc0082375c1055cf8cb75d7779f8dad8d010c0
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-03 19:36:48 +0700

    add common service

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommonService {
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  callHttpPost(url: string, payload: any) {
+    console.log(url, payload);
+    return this.httpClient.post(url, payload);
+  }
+
+  callHttpPut(url: string, payload: any) {
+    return this.httpClient.put(url, payload);
+  }
+
+  callHttpGet(url: string, payload: any) {
+    return this.httpClient.get(url);
+  }
+
 }
