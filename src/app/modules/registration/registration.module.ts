1aeaf13d0afc53307876df8e649859c43e61aa8d
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-03 16:36:18 +0700

    add shared module to registration module

--- src/app/modules/registration/registration.module.ts
+++ src/app/modules/registration/registration.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 const routes: Routes = [
   { path: '', component: RegistrationComponent }
@@ -11,6 +12,7 @@ const routes: Routes = [
   declarations: [RegistrationComponent],
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes),
   ]
 })
