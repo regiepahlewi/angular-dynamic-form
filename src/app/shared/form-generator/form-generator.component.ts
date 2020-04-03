5e329185685daf5d3d16810dbda1db3ab61bad9f
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-03 13:47:27 +0700

    add form generator component

---
+++ src/app/shared/form-generator/form-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-generator',
+  templateUrl: './form-generator.component.html',
+  styleUrls: ['./form-generator.component.scss']
+})
+export class FormGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
