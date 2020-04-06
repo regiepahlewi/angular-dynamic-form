449b3476c7eefbf3fe9ff6a053befeb0b72c648b
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 22:55:32 +0700

    add common date, day, month

--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ISelectOptions } from '../interfaces/field';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,62 @@ export class CommonService {
 
   callHttpPost(url: string, payload: any) {
     return this.httpClient.post(url, payload);
+  }
+
+  getMonth(): ISelectOptions[] {
+    const month: ISelectOptions[] = [
+      { key: '01', value: 'January' },
+      { key: '02', value: 'February' },
+      { key: '03', value: 'March' },
+      { key: '04', value: 'April' },
+      { key: '05', value: 'May' },
+      { key: '06', value: 'June' },
+      { key: '07', value: 'July' },
+      { key: '08', value: 'August' },
+      { key: '09', value: 'September' },
+      { key: '10', value: 'October' },
+      { key: '11', value: 'November' },
+      { key: '12', value: 'December' },
+    ]
+    return month;
+  }
+
+  getDay(): ISelectOptions[] {
+    const day: ISelectOptions[] = [];
+
+    for (let i = 1; i <= 31; i++) {
+      if (i <= 9) {
+        const addZero = '0' + i.toString();
+        day.push({
+          key: addZero,
+          value: addZero
+        });
+      } else {
+        day.push({
+          key: i,
+          value: i.toString()
+        });
+      }
+    }
+
+    return day;
+
+  }
+
+  getYear(interval: number): ISelectOptions[] {
+    const year: ISelectOptions[] = [];
+    const yearNow = new Date().getFullYear();
+    const yearInterval = yearNow - interval;
+
+    for (let i = yearInterval; i <= yearNow; i++) {
+      year.push({
+        key: i,
+        value: i.toString()
+      });
+    }
+
+    year.sort();
+
+    return year;
   }
 }
