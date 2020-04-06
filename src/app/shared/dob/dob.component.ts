243a916cc887eb4ddc19ea446a7a089e3450bfad
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 22:55:53 +0700

    implement to dob component

--- src/app/shared/dob/dob.component.ts
+++ src/app/shared/dob/dob.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IField } from 'src/app/interfaces/field';
 import { FormGroup } from '@angular/forms';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-dob',
@@ -12,35 +13,17 @@ export class DobComponent implements OnInit {
   field: IField;
   group: FormGroup;
   required: boolean;
-  day = [
-    { key: '01', value: 1 },
-    { key: '02', value: 2 },
-    { key: '03', value: 3 },
-    { key: '04', value: 4 },
-    { key: '05', value: 5 },
-  ];
-
-  month = [
-    { key: '01', value: 'January' },
-    { key: '02', value: 'February' },
-    { key: '03', value: 'March' },
-    { key: '05', value: 'April' },
-    { key: '06', value: 'May' },
-  ];
-
-  year = [
-    { key: '2009', value: 2009 },
-    { key: '2010', value: 2010 },
-    { key: '2011', value: 2011 },
-    { key: '2012', value: 2012 },
-    { key: '2013', value: 2013 },
-  ];
+  day = this.commonService.getDay();
+  month = this.commonService.getMonth();
+  year = this.commonService.getYear(17);
 
   selectedDay: string;
   selectedMonth: string;
   selectedYear: string;
 
-  constructor() { }
+  constructor(
+    private commonService: CommonService
+  ) { }
 
   ngOnInit(): void {
     if (this.group.controls[this.field.name].value !== null) {
