f17fab6f8f709399d1d94a7f7fcc07cc13ef21ad
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 08:55:16 +0700

    add provider and imports unit test regisration

--- src/app/modules/registration/registration.component.spec.ts
+++ src/app/modules/registration/registration.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegistrationComponent } from './registration.component';
+import { CommonService } from 'src/app/services/common.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('RegistrationComponent', () => {
   let component: RegistrationComponent;
@@ -8,7 +10,13 @@ describe('RegistrationComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
+      declarations: [ RegistrationComponent ],
+      imports: [
+        HttpClientModule
+      ],
+      providers: [
+        CommonService
+      ]
     })
     .compileComponents();
   }));
