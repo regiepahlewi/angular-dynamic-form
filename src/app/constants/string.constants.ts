98b3b8fbea24f2bfd7aa291fb02de326ba8f6508
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 15:07:10 +0700

    email regex

--- src/app/constants/string.constants.ts
+++ src/app/constants/string.constants.ts
@@ -2,5 +2,5 @@
 export class StringConstants {
     // REGEX
     static REGEX_PHONE_NUMBER_INA = '^[+]*[(]{0,1}[6]{0,2}[2]{0,3}[8]{1,4}[)]{0,1}[0-9]*$';
-    static REGEX_EMAIL = '';
+    static REGEX_EMAIL = '^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$';
 }
