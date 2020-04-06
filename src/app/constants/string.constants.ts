3212413a51b1eef0d227865dfbbac1b091d8bbd6
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 13:49:14 +0700

    add string and webaddress constant

--- src/app/constants/string.constants.ts
+++ src/app/constants/string.constants.ts
@@ -1,5 +1,6 @@
 
-export class WebAddressConstant {
-    static API_CONFIG = 'http://localhost:3000/';
-    static REGISTRATION_SAVE = WebAddressConstant.API_CONFIG + 'registration';
+export class StringConstants {
+    // REGEX
+    static REGEX_PHONE_NUMBER_INA = '^[+]*[(]{0,1}[6]{0,2}[2]{0,3}[8]{1,4}[)]{0,1}[0-9]*$';
+    static REGEX_EMAIL = '';
 }
---
+++ src/app/constants/webaddress.constants.ts
@@ -0,0 +1,7 @@
+export class WebAddressConstant {
+    // API CONFIG
+    static API_CONFIG = 'http://localhost:3000/';
+
+    // REGISTRATION
+    static REGISTRATION_SAVE = WebAddressConstant.API_CONFIG + 'registration';
+}
