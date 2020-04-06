a50062d5571815e970ae2e9ccb90f64b8f42374b
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 08:54:41 +0700

    common service unit test add providers

--- src/app/services/common.service.spec.ts
+++ src/app/services/common.service.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CommonService } from './common.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('CommonService', () => {
   let service: CommonService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        HttpClient,
+        HttpHandler
+      ]
+    });
     service = TestBed.inject(CommonService);
   });
 
