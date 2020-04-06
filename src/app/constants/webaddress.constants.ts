6bffd7e3dafee0504336a110a22b96c28a5c2566
Regie Pahlewi <regiealgipahlewi@gmail.com>
2020-04-06 15:08:00 +0700

    change API to empty string, handle by proxy

--- src/app/constants/webaddress.constants.ts
+++ src/app/constants/webaddress.constants.ts
@@ -1,6 +1,7 @@
 export class WebAddressConstant {
     // API CONFIG
-    static API_CONFIG = 'http://localhost:3000/';
+    // static API_CONFIG = 'http://localhost:3000/';
+    static API_CONFIG = '';
 
     // REGISTRATION
     static REGISTRATION_SAVE = WebAddressConstant.API_CONFIG + 'registration';
